3574a2465c82f76b65a64db4370fc8d9d881c995
Matt <mattjmcguiness@gmail.com>
2020-05-13 07:59:28 -0400

    Get ENV vars via os.environ syntax

--- app/main/config.py
+++ app/main/config.py
@@ -8,25 +8,25 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URI')
+    SECRET = os.environ['SECRET']
+    SQLALCHEMY_DATABASE_URI = os.environ['PROD_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ['DEV_DATABASE_URI']
 
 
 class LocalConfig(Config):
     """Configurations for local development"""
     ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ['LOCAL_DATABASE_URI']
 
 
 class TestingConfig(Config):
@@ -34,7 +34,7 @@ class TestingConfig(Config):
     ENV = 'testing'
     DEBUG = False
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ['TEST_DATABASE_URI']
 
 
 class ProductionConfig(Config):
